f107550b707105dff11c4cff8323d8203c3d2622
A1eKs94 <128930170+A1eKs94@users.noreply.github.com>
2024-10-22 20:30:36 -0700

    sin terminar 15

---
+++ app/authController.php
@@ -0,0 +1,47 @@
+<?php
+
+    session_start();
+
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $newUser = new User();
+
+    $newUser -> login($email, $password);
+
+
+    class User{
+
+        function login($email, $password){
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('email' => $email,'password' => $password),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            
+            $response = json_decode($response);
+
+            if ($response['code'] == 2) {
+                $_SESSION['data'] = $response;
+                header('Location: ../home.html');
+            
+            }
+
+        }
+
+    }
+
+
+?>
