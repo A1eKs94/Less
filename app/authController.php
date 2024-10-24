462ecfc71d479b56f524f80bc7089be021a97a33
Aleksey Unzon <rubenalekseyunzon@gmail.com>
2024-10-23 19:10:45 -0700

    Actividad-15

--- app/authController.php
+++ app/authController.php
@@ -7,7 +7,7 @@
 
     $newUser = new User();
 
-    $newUser -> login($email, $password);
+    $newUser->login($email, $password);
 
 
     class User{
@@ -16,32 +16,31 @@
             $curl = curl_init();
 
             curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => array('email' => $email,'password' => $password),
+                CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array('email' => $email, 'password' => $password),
             ));
 
             $response = curl_exec($curl);
 
             curl_close($curl);
-            
+
             $response = json_decode($response);
 
-            if ($response['code'] == 2) {
+            if (isset($response->code) && $response->code == 2) {
                 $_SESSION['data'] = $response;
                 header('Location: ../home.html');
-            
+                exit(); 
+            } else {
+                echo "Login failed. Please check your credentials.";
             }
-
         }
-
     }
-
 
 ?>
